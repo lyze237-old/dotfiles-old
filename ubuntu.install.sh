2359789494d0fc68eb386a659262cda56b0e4f15
Lyze <11274700+lyze237@users.noreply.github.com>
2018-03-05 19:20:35 +0100

    added pass stuff

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -51,7 +51,7 @@ echo Updating apt cache
 sudo apt update
 
 echo Installing everything possible
-sudo apt install -f -y xorg corebird fish python3-setuptools arc-theme numix-icon-theme-circle mopidy ncmpcpp rofi g++ qttools5-dev-tools build-essential qt5-qmake qt5-default dotnet-sdk-2.0.* mono-devel xdotool htop indicator-kdeconnect nextcloud-client compton mopidy-spotify steam mpc mpdris2 redshift redshift-gtk ffmpeg obs-studio mpv vlc python3-socks gimp wmctrl feh awesome i3lock arandr lxappearance ranger w3m xfce4-terminal thunar unzip pulseaudio pavucontrol thunar scrot imagemagick libnotify-bin ubuntu-restricted-extras urlview xclip weechat neovim numlockx thunderbird breeze-cursor-theme firefox python3-docopt python3-jinja2 xarchiver urlview w3m gpgsm msmtp offlineimap mutt mousepad neovim
+sudo apt install -f -y xorg corebird fish python3-setuptools arc-theme numix-icon-theme-circle mopidy ncmpcpp rofi g++ qttools5-dev-tools build-essential qt5-qmake qt5-default dotnet-sdk-2.0.* mono-devel xdotool htop indicator-kdeconnect nextcloud-client compton mopidy-spotify steam mpc mpdris2 redshift redshift-gtk ffmpeg obs-studio mpv vlc python3-socks gimp wmctrl feh awesome i3lock arandr lxappearance ranger w3m xfce4-terminal thunar unzip pulseaudio pavucontrol thunar scrot imagemagick libnotify-bin ubuntu-restricted-extras urlview xclip weechat neovim numlockx thunderbird breeze-cursor-theme firefox python3-docopt python3-jinja2 xarchiver urlview w3m gpgsm msmtp offlineimap mutt mousepad neovim pass
 
 #############################################
 # Manual Website Installation
@@ -180,3 +180,13 @@ sudo mkdir -p /lib/terminfo/x; sudo ln -s \
 /usr/local/share/terminfo/x/xterm-termite \
 /lib/terminfo/x/xterm-termite
 
+# pass stuff
+cd /tmp
+git clone https://github.com/roddhjav/pass-update/
+cd pass-update
+sudo make install
+
+cd /tmp
+git clone https://github.com/tadfisher/pass-otp
+cd pass-otp
+sudo make install
