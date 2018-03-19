5771453982f8a761b89d123cc6ac3e3c14217188
Lyze <11274700+lyze237@users.noreply.github.com>
2018-03-19 17:00:52 +0100

    added broken vimdiff

--- mac.install.sh
+++ mac.install.sh
@@ -4,5 +4,7 @@ echo Download and install iterm2 from https://iterm2.com/downloads.html
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install urlview
+brew install w3m
 brew install neomutt
 brew install terminal-notifier
