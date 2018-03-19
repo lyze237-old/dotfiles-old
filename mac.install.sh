5343734792f855e5b55ca81a691a5020ac9faeef
Lyze <11274700+lyze237@users.noreply.github.com>
2018-03-19 15:39:29 +0100

    updated mutt stuff for macs

---
+++ mac.install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Download and install iterm2 from https://iterm2.com/downloads.html
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install neomutt
+brew install terminal-notifier
