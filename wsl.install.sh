065927a701581536b77821c1c546c7106de27c15
Lyze <11274700+lyze237@users.noreply.github.com>
2018-03-28 19:53:40 +0200

    updated wsl install

--- wsl.install.sh
+++ wsl.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install urlview build-essentials git libgnutls-dev libcurl4-gnutls-dev gnutls-bin autoconf libncursesw5-dev libssl-dev libsasl2-dev libtokyocabinet-dev links libssl-dev libsasl2-dev xsltproc
+sudo apt install urlview build-essentials git libgnutls-dev libcurl4-gnutls-dev gnutls-bin autoconf libncursesw5-dev libssl-dev libsasl2-dev libtokyocabinet-dev links libssl-dev libsasl2-dev xsltproc w3m
 
 cd /tmp#
 git clone https://github.com/neomutt/neomutt
