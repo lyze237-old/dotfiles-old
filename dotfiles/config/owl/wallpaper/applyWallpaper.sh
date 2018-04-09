1618719cdd099a965169f0ee7ec3249a717c224c
Lyze <11274700+lyze237@users.noreply.github.com>
2018-04-09 19:12:27 +0200

    updated stuff

--- dotfiles/config/owl/wallpaper/applyWallpaper.sh
+++ dotfiles/config/owl/wallpaper/applyWallpaper.sh
@@ -2,5 +2,9 @@
 
 
 for index in 0 2 2 2 2 ; do
-	sleep $index && feh --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' &
+#	sleep $index && feh --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' --bg-scale '/home/lyze/.config/owl/wallpaper/new.png' &
+
+    sleep $index && feh --bg-scale '/home/lyze/.config/owl/wallpaper/middle.png' --bg-scale '/home/lyze/.config/owl/wallpaper/left.png' --bg-scale '/home/lyze/.config/owl/wallpaper/right.png' &
+
+
 done
