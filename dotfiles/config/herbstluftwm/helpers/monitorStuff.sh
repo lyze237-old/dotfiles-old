2737e5d2a44530cedf898895bc037e42f2c0a0e2
Lyze <11274700+lyze237@users.noreply.github.com>
2018-05-11 10:50:33 +0200

    split up monitor stuff, added correct floating window placement with automated programs and proper closing of window placements

--- dotfiles/config/herbstluftwm/helpers/monitorStuff.sh
+++ dotfiles/config/herbstluftwm/helpers/monitorStuff.sh
@@ -1,11 +1,42 @@
 #!/usr/bin/env bash
 
+source $HOME/.config/herbstluftwm/variables
+source $HOME/.config/herbstluftwm/helpers/hc.sh
+
+function getActiveMonitorRegion() {
+    getMonitorRegion
+}
+
 function getMonitorCount() {
     hc list_monitors | wc -l
 }
 
-function getActiveMonitorRegion() {
-    getMonitorRegion
+function getMonitorOfWindow() {
+    winId="$1"
+    eval $(xdotool getwindowgeometry --shell $winId)
+    # X, Y, WIDTH, HEIGHT => window coordinates
+
+    X=$(($X + $WIDTH / 2))
+    Y=$(($Y + $HEIGHT/ 2))
+
+    while read -r line ; do
+        monitorId=$(echo "$line" | cut -d':' -f1)
+        monitorName=$(echo "$line" | cut -d' ' -f7)
+        if [[ "$monitorName" == "\"$floatingMonitorName\"" ]] ; then
+            continue
+        fi
+
+        # X, Y => window middle 
+        # MonitorX, MonitorY, MonitorWidth, MonitorHeight => monitor coordinates
+        eval $(getMonitorRegion $monitorId)
+        if [[ $X -lt $(($MonitorX + $MonitorWidth)) && \
+            $X -gt $MonitorX && \
+            $Y -gt $MonitorY && \
+            $Y -lt $(($MonitorY + $MonitorHeight)) ]] ; then
+            echo $monitorId
+            break
+        fi
+    done <<< $(hc list_monitors)
 }
 
 function getMonitorRegion() {
@@ -15,5 +46,14 @@ function getMonitorRegion() {
     echo MonitorY=$(echo $monRect | cut -d" " -f2)
     echo MonitorWidth=$(echo $monRect | cut -d" " -f3)
     echo MonitorHeight=$(echo $monRect | cut -d" " -f4)
+}
+
+function shiftWindowToMonitor() {
+    windowId="$1"
+    monitorId="$(getMonitorOfWindow $windowId)"
+
+    echo Shifting $windowId to $monitorId
+    
+    herbstclient chain , jumpto $windowId , shift_to_monitor $monitorId
 }
 
