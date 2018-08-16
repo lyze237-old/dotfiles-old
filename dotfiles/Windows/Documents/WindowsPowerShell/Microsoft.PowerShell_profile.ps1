6a2d65ca800e77a4c2d293d20e6bc78c65fd494f
Lyze <11274700+lyze237@users.noreply.github.com>
2018-08-16 12:58:26 +0200

    added colored ls

--- dotfiles/Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
 Import-Module PSReadLine
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
+Import-Module Get-ChildItemColor
+
+Set-Alias ls Get-ChildItemColor -option AllScope
+
 function Prompt
 {
     $mywd = (Get-Location).Path
