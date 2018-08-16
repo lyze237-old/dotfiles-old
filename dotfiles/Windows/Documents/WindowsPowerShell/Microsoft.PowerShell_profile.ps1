3d72d1e3707977240fc47fa7db5f4c965472cfd8
Lyze <11274700+lyze237@users.noreply.github.com>
2018-08-16 19:10:20 +0200

    added copyright notice script to pwsh

--- dotfiles/Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -46,7 +46,7 @@ function Prompt
 
     Write-Host "/" -NoNewline -ForegroundColor Cyan;
     Write-Host "`"" -NoNewline -ForegroundColor Red;
-    Write-Host "/ " -NoNewline -ForegroundColor Cyan;
+    Write-Host "/" -NoNewline -ForegroundColor Cyan;
 
     # restore exit code
     $global:LASTEXITCODE = $properExitCode;
@@ -63,4 +63,55 @@ function Test-Administrator
 {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+}
+
+function Write-Copyright
+{
+    param($target = (Get-Location).Path)
+
+    # Header template
+    $header = "//-----------------------------------------------------------------------
+// <copyright>
+// `"THE BEER-WARE LICENSE`" (Revision 42):
+// <lyze@owl.sh> wrote this file. As long as you retain this notice you
+// can do whatever you want with this stuff. If we meet some day, and you think
+// this stuff is worth it, you can buy me a beer in return Michael Weinberger
+// </copyright>
+//-----------------------------------------------------------------------`r`n"
+
+    function Write-Header ($file)
+    {
+        # Get the file content as as Array object that contains the file lines
+        $content = Get-Content $file
+        
+        # Getting the content as a String
+        $contentAsString =  $content | Out-String
+        
+        <# If content starts with // then the file has a copyright notice already
+        Let's Skip the first 14 lines of the copyright notice template... #>
+        if($contentAsString.StartsWith("//"))
+        {
+        $content = $content | Select-Object -skip 14
+        }
+
+        # Splitting the file path and getting the leaf/last part, that is, the file name
+        $filename = Split-Path -Leaf $file
+
+        # $fileheader is assigned the value of $header with dynamic values passed as parameters after -f
+        $fileheader = $header
+
+        # Writing the header to the file
+        Set-Content $file $fileheader -encoding UTF8
+
+        # Append the content to the file
+        Add-Content $file $content
+    }
+
+    #Filter files getting only .cs ones and exclude specific file extensions
+    Get-ChildItem $target -Filter *.cs -Exclude *.Designer.cs,T4MVC.cs,*.generated.cs,*.ModelUnbinder.cs -Recurse | % `
+    {
+        <# For each file on the $target directory that matches the filter,
+        let's call the Write-Header function defined above passing the file as parameter #>
+        Write-Header $_.PSPath.Split(":", 3)[2]
+    }
 }
