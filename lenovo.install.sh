8187a1536a754cccb6c753abf5c2c4fb4200f9ec
Lyze <11274700+lyze237@users.noreply.github.com>
2018-11-13 21:15:59 +0100

    added comments, added dotnet.script

--- lenovo.install.sh
+++ lenovo.install.sh
@@ -11,15 +11,18 @@ sudo apt install neovim gnome-tweak-tool evolution chrome-gnome-shell git fish s
 
 sudo apt remove thunderbird
 
+# installs gnome terminal colors
 cd /tmp
 git clone https://github.com/metalelf0/gnome-terminal-colors.git
 cd gnome-terminal-colors
 ./install.sh -s gruvbox-dark
 cd ~
 
+# sets fish shell
 chsh -s `which fish`
 sudo chsh -s `which fish`
 
+# installs dotnet sdk
 cd /tmp
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -27,6 +30,10 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install dotnet-sdk-2.1
 
-echo install vs code and jetbrains toolbox manually
+# installs dotnet-script
+curl -s https://raw.githubusercontent.com/filipw/dotnet-script/master/install/install.sh | bash
 
+# install vs code extensions
 ./dotfiles/Commands/InstallVsCodeExtensions.ps1
+
+echo install vs code and jetbrains toolbox manually
