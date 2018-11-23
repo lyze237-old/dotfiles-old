6e6a16884d4e896b072a371f524ccdfddf70ae0f
Lyze <11274700+lyze237@users.noreply.github.com>
2018-11-23 15:58:14 +0100

    added libinput

--- lenovo.install.sh
+++ lenovo.install.sh
@@ -3,23 +3,25 @@
 # ubuntu 18.10 gnome edition
 
 
-sudo snap install vlc spotify discord gimp inkscape telegram-desktop bitwarden bw 
+sudo snap install vlc spotify telegram-desktop bitwarden bw 
 sudo snap install --classic powershell-preview
 sudo snap alias powershell-preview pwsh
 
-sudo apt install neovim gnome-tweak-tool evolution chrome-gnome-shell git fish scdaemon curl
+sudo apt install neovim gnome-tweak-tool evolution chrome-gnome-shell git fish scdaemon curl inkscape vlc gimp
 
 sudo apt remove thunderbird
 
+# gruvbox gnome terminals
 cd /tmp
 git clone https://github.com/metalelf0/gnome-terminal-colors.git
 cd gnome-terminal-colors
 ./install.sh -s gruvbox-dark
-cd ~
 
+# fish shell
 chsh -s `which fish`
 sudo chsh -s `which fish`
 
+# dotnet sdk
 cd /tmp
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -27,6 +29,19 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install dotnet-sdk-2.1
 
-echo install vs code and jetbrains toolbox manually
+# libinput-gestures
+cd /tmp
+sudo gpasswd -a $USER input
+sudo apt-get install xdotool wmctrl libinput-tools
+git clone https://github.com/bulletmark/libinput-gestures.git
+cd libinput-gestures
+sudo make install
+libinput-gestures-setup autostart
+libinput-gestures-setup start &
+
+echo install the following programs manually: 
+echo vs code
+echo jetbrains toolbox
+echo discord
 
 ./dotfiles/Commands/InstallVsCodeExtensions.ps1
